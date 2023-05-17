2477ee60f1feb38e3396a4a0d875815b779bacb2
victorhorton <vichorton5@gmail.com>
2023-05-16 20:52:42 -0500

    Finish Vue setup through importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "vue", to: "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "main"
