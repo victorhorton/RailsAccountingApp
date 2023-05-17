131e9ca1f5f0ae158aa5e11d04a116ef6e7c9783
victorhorton <vichorton5@gmail.com>
2023-05-16 23:34:26 -0500

    Add jQuery for AJAX calls

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "vue", to: "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "main"
