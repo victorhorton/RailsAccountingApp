6128c0ddb70699b5cde5337334b468686d6b08ef
victorhorton <vichorton5@gmail.com>
2023-05-16 21:36:21 -0500

    Add missing commas to model enums

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -3,7 +3,7 @@ class Batch < ApplicationRecord
 	has_many :tranzactions
 	has_many :entries, through: :tranzactions
 
-	enum :purpose {
+	enum :purpose, {
 		accounts_payable: 0,
 		accounts_receivable: 1,
 		general_ledger: 2,
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,7 @@ class Entry < ApplicationRecord
 
 	belongs_to :tranzaction
 
-	enum :entry_type {
+	enum :entry_type, {
 		debit: 0,
 		credit: 1,
 	}
