90901faf350990ca0e64a6df533fa9ed9babb110
victorhorton <vichorton5@gmail.com>
2023-05-16 23:37:30 -0500

    Add basic batch index, create, edit and update actions/views

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,2 +1,47 @@
 class BatchesController < ApplicationController
+	def index
+		@batches = Batch.eager_load(tranzactions: :entries).all
+	end
+
+	def create
+		@batch = Batch.new(batch_params)
+
+		if @batch.save
+			redirect_to edit_batch_path(@batch)
+		else
+			redirect_to batches_path
+		end
+	end
+
+  def update
+    @batch = Batch.find(params[:id])
+
+    if @batch.update(batch_params)
+      redirect_to edit_batch_path(@batch)
+    else
+      redirect_to batches_path
+    end
+  end
+
+  def edit
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  {
+        @batch = Batch.eager_load(tranzactions: :entries).find(params[:id])
+        render json: @batch, serializer: BatchSerializer
+      }
+    end
+  end
+
+	private
+
+	def batch_params
+		params.require(:batch).permit(
+			:id,
+			:name,
+			:comment,
+			:purpose,
+			:posted_at,
+		)
+	end
 end
