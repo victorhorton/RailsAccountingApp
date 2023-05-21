e8c0278f92c620fd785abef343ff95353a50ad33
victorhorton <vichorton5@gmail.com>
2023-05-20 21:20:49 -0500

    Add validation to batches

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -5,6 +5,11 @@ class Batch < ApplicationRecord
 
 	accepts_nested_attributes_for :tranzactions
 
+	validates_presence_of :name, :purpose
+	validates_associated :tranzactions, message: lambda { |obj_class, obj|
+		return obj[:value].map{|t| t.errors.full_messages}.flatten.compact.join(', ')
+	}
+
 	enum :purpose, {
 		accounts_payable: 0,
 		accounts_receivable: 1,
