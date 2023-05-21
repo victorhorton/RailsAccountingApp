97c1b67ee5bec35f016b19563d93d7f97b5737f1
victorhorton <vichorton5@gmail.com>
2023-05-21 15:51:56 -0500

    Remove turbo links

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+# pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "vue", to: "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
