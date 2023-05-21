2abdc4614d9b91e3123a8cafd01138ed8765c18c
victorhorton <vichorton5@gmail.com>
2023-05-20 21:20:40 -0500

    Add validation to tranzactions

--- app/models/tranzaction.rb
+++ app/models/tranzaction.rb
@@ -3,4 +3,32 @@ class Tranzaction < ApplicationRecord
 	has_many :entries
 
 	accepts_nested_attributes_for :entries
+
+	validates_presence_of :company_id, :date
+	validate :entries_cancel?
+	validates_associated :entries, message: lambda { |obj_class, obj|
+		return obj[:value].map{|t| t.errors.full_messages}.flatten.compact.join(', ')
+	}
+
+	def credit_entries
+		entries.select{|entry| entry.entry_type == 'credit'}
+	end
+
+	def debit_entries
+		entries.select{|entry| entry.entry_type == 'debit'}
+	end
+
+	def credit_total
+		credit_entries.pluck(:amount).compact.sum
+	end
+
+	def debit_total
+		debit_entries.pluck(:amount).compact.sum
+	end
+
+	private
+
+	def entries_cancel?
+		errors.add(:base, "Transaction doesn't balance") if credit_total != debit_total
+	end
 end
