d7fb622e125dc8085b22edd3bed824fca1b99ee0
victorhorton <vichorton5@gmail.com>
2023-05-24 18:02:25 -0500

    Set default values based on batch purpose

--- app/serializers/directed_batch_serializer.rb
+++ app/serializers/directed_batch_serializer.rb
@@ -1,11 +1,23 @@
 class DirectedBatchSerializer < ActiveModel::Serializer
-  attributes :id, :company_id, :date, :entries_attributes
+  attributes :id, :company_id, :date, :entries_attributes, :batch_id
 
   def entries_attributes
+
+    batch_purpose = Batch.find(object.batch_id).purpose
+    if batch_purpose == 'payable'
+      primary_account_id = 2010
+      primary_type = 'credit'
+      reversed_type = 'debit'
+    else
+      primary_account_id = 2010
+      primary_type = 'debit'
+      reversed_type = 'credit'
+    end
+
     if object.entries.blank?
       return [
-        {designation: 'primary', position: 1, account_id: 2010, entry_type: 'credit'},
-        {designation: 'distribution', position: 2, entry_type: 'debit'}
+        {designation: 'primary', position: 1, account_id: primary_account_id, entry_type: primary_type},
+        {designation: 'distribution', position: 2, entry_type: reversed_type}
       ]
     else
       return object.entries.map{ |entry|
