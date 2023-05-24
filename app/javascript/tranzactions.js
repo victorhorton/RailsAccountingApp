aad837f90f3cb8eb3f65c0f880b5198b344716f0
victorhorton <vichorton5@gmail.com>
2023-05-23 23:45:52 -0500

    Add Vue methods and computed values for tranzaction new

--- app/javascript/tranzactions.js
+++ app/javascript/tranzactions.js
@@ -33,7 +33,70 @@ import * as common from 'common'
       }
     },
     methods: {
+      getAmount(entry) {
+        return common.parseAmount(entry.amount);
+      },
+      setAmount(entry, entryType) {
+        const entryAmount = parseFloat(event.currentTarget.value);
+
+        if (entryAmount || entryAmount == 0) {
+          entry.entry_type = entryType;
+          entry.amount = entryAmount;
+        } else {
+          return
+        }
+      },
+      getDate(tranzaction) {
+        return common.parseDate(tranzaction.date);
+      },
+      setDate(tranzaction) {
+        const date = event.currentTarget.value;
+        tranzaction.date = common.formatDate(date);
+      },
+      getCompany(tranzaction) {
+        if (!tranzaction.company_id) {
+          return null
+        }
+        return this.companies.find(c => c.id === tranzaction.company_id).code
+      },
+      setCompany(tranzaction) {
+        const companyCode = event.currentTarget.value.toUpperCase();
+        const company = this.companies.find(c => c.code === companyCode);
+        this.tranzaction.company_id = company.id;
+      },
+      submitForm() {
+        const isNew = railsParams.action === 'new'
+        const url = isNew ? '/tranzactions' : `/tranzactions/${this.tranzaction.id}`;
+        const type = isNew ? 'POST' : 'PATCH';
+        $.ajax({
+          url,
+          type,
+          dataType: 'json',
+          headers: {
+            "X-CSRF-Token":  $('[name=csrf-token]')[0].content,
+          },
+          data: {
+            tranzaction: this.tranzaction
+          },
+          success:  e => {
+            window.location = `/batches?purpose=${this.batch.purpose}`
+          },
+          error:  e => {
+            location.reload()
+          }
+        });
+      }
     },
     computed: {
+      primaryEntry() {
+        return this.tranzaction.entries_attributes.find(entry => {
+          return entry.designation === 'primary';
+        })
+      },
+      distributionEntries() {
+        return this.tranzaction.entries_attributes.filter(entry => {
+          return entry.designation === 'distribution';
+        })
+      }
     }
   }).mount('#vue-tranzactions')
