f04164ca874d7604565e62b4006605cc67d794a4
victorhorton <vichorton5@gmail.com>
2023-05-25 18:16:23 -0500

    Preload js frameworks and libraries in importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,9 @@ pin "application", preload: true
 # pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "vue", to: "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+pin "vue", to: "https://unpkg.com/vue@3/dist/vue.esm-browser.js", preload: true
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js", preload: true
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "common"
 pin "batches"
