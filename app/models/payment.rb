fe652a92eea4d1bc79d4cda09949d2f599fc2a06
victorhorton <vichorton5@gmail.com>
2023-05-26 00:25:15 -0500

    Add payment_type enum values to model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,4 +3,12 @@ class Payment < ApplicationRecord
   belongs_to :tranzaction
 
   accepts_nested_attributes_for :tranzaction
+
+  enum :payment_type, {
+    check: 0,
+    wire: 1,
+    credit_card: 2,
+  }
+
+
 end
