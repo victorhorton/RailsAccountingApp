7a318a29ed0e2660ad8e69feab2db099e01585ce
victorhorton <vichorton5@gmail.com>
2023-05-27 16:34:13 -0500

    Add tranzaction contact association

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
   validates_presence_of :name
+  has_many :tranzactions
 end
--- app/models/tranzaction.rb
+++ app/models/tranzaction.rb
@@ -3,6 +3,7 @@ class Tranzaction < ApplicationRecord
 	
 	belongs_to :batch
 	belongs_to :company
+	belongs_to :contact, optional: true
 	has_one :payment, dependent: :destroy
 	has_many :entries, dependent: :destroy
 	has_and_belongs_to_many :payments
@@ -10,6 +11,7 @@ class Tranzaction < ApplicationRecord
 	accepts_nested_attributes_for :entries
 	accepts_nested_attributes_for :payments
 
+	validates_presence_of :contact_id, if: :not_gl
 	validates_presence_of :company_id, :date
 	validate :entries_cancel?
 	validates_associated :entries, message: lambda { |obj_class, obj|
@@ -45,6 +47,10 @@ class Tranzaction < ApplicationRecord
 	end
 
 	private
+
+	def not_gl
+		batch.purpose != 'general_ledger'
+	end
 
 	def entries_cancel?
 		errors.add(:base, "Transaction doesn't balance") if credit_total != debit_total
