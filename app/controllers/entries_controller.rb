b21f1e3f0e9033204934709069839adfc3cfd502
victorhorton <vichorton5@gmail.com>
2023-05-29 11:16:47 -0500

    Add trial balance js and routes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,25 @@
 class EntriesController < ApplicationController
+
   def destroy
     entry = Entry.find(params[:id])
     entry.destroy
+  end
+
+  def trial_balance
+    respond_to do |format|
+      format.html {
+        trial_balance_breadcrumbs
+      }
+      format.json {
+        render json: TrialBalanceDatatable.new(params, view_context: view_context)
+      }
+    end
+  end
+
+  private
+
+  def trial_balance_breadcrumbs
+    add_breadcrumb "Home", :root_path
+    add_breadcrumb "Trial Balance", :trial_balance_entries_path
   end
 end
---
+++ app/javascript/trial_balance.js
@@ -0,0 +1,51 @@
+import * as common from 'common'
+
+$(document).ready(function() {
+  const trialBalanceDatatable = $('#trial-balance-datatable').DataTable({
+    "processing": true,
+    "serverSide": true,
+    "ajax": {
+      "url": $('#trial-balance-datatable').data('source')
+    },
+    footerCallback: function ( tfoot, data, start, end, display ) {
+      const tableInfo = trialBalanceDatatable.page.info();
+      const api = this.api();
+
+      const debitColumn = api.column( 6 );
+      const creditColumn = api.column( 7 );
+
+      if (tableInfo.recordsDisplay != tableInfo.end) {
+        $(debitColumn.footer()).html('');
+        $(creditColumn.footer()).html('');
+        return
+      }
+
+      const debit_total = common.sumTotals(debitColumn.data());
+      const credit_total = common.sumTotals(creditColumn.data());
+
+      $(debitColumn.footer()).html(`Debits: ${debit_total}`);
+      $(creditColumn.footer()).html(`Credits: ${credit_total}`);
+    },
+    "pagingType": "full_numbers",
+    "columns": [
+      {"data": "account_id"},
+      {"data": "purpose"},
+      {"data": "contact"},
+      {"data": "date"},
+      {"data": "reference_number"},
+      {"data": "company"},
+      {"data": "debit"},
+      {"data": "credit"},
+    ]
+    // pagingType is optional, if you want full pagination controls.
+    // Check dataTables documentation to learn more about
+    // available options.
+  }).columns().every(function () {
+    const datatableColumns = this;
+    $('input', this.header()).on('keyup change', function () {
+      if (datatableColumns.search() !== this.value) {
+        datatableColumns.search(this.value).draw();
+      }
+    });
+  });
+});
