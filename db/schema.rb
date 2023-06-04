486f0c9bb77b01d286f3eae71b1381b0aeaeeea0
victorhorton <vichorton5@gmail.com>
2023-06-04 15:56:01 -0500

    Add entry description field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_033333) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_053819) do
   create_table "accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.integer "account_type"
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_033333) do
     t.integer "entry_type"
     t.decimal "amount", precision: 18, scale: 2
     t.integer "designation"
+    t.string "description"
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
